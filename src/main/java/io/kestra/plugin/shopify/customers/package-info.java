a058d1f865b6e1bcd05b5ba4c18c572733560e9a
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 17:51:12 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/shopify/customers/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Shopify Customer",
+    description = "This sub-group of plugins contains tasks to interact with Shopify Customers.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
+)
+package io.kestra.plugin.shopify.customers;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
