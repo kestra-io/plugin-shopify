7613a2ba65da5c5eaa97c4e1131b66d7d457dd4e
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 17:51:12 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/shopify/orders/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Shopify Order",
+    description = "This sub-group of plugins contains tasks to interact with Shopify Orders.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
+)
+package io.kestra.plugin.shopify.orders;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ src/main/java/io/kestra/plugin/shopify/products/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Shopify Order",
+    description = "This sub-group of plugins contains tasks to interact with Shopify Products.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
+)
+package io.kestra.plugin.shopify.products;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
